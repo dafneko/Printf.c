02fab223f33ad124696f1f8a8ba1ff3e9de737f3
dafneko <124280278+dafneko@users.noreply.github.com>
2024-01-08 16:42:26 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -57,35 +57,33 @@ int	ft_printf(const char *str, ...)
 	va_end(ptr);
 	return (count);
 }
-
+/*
 int	main(void)
 {
 	int count = 0;
 	int mine = 0;
 	char *p = "Hi";
 	char *ptr = "Wow";
-	// printf("Address = %p\n", p);
-	// mine += ft_printf("this is mine %p\n", p);
-	// count += printf("this is orig %p\n", p);
-	// printf("org = %i\n", count);
-	// mine += ft_printf("this is mine %X %s %i %d %u %x %c %% %p\n", 1234,"Hi", 45, 00, -10, 04, 'h', p);
-	// count += printf("this is orig %X %s %i %d %u %x %c %% %p\n", 1234, "Hi", 45, 00, -10, 04, 'h', p);
-	// mine += ft_printf("this is mine %X %X\n", 1234, -1234);
-	// count += printf("this is orig %X %X\n", 1234, -1234);
-	// mine += ft_printf("this is mine %x %x\n", 1234, -1234);
-	// count += printf("this is orig %x %x\n", 1234, -1234);
-	// mine += ft_printf("this is mine %p %p\n", ptr, p);
-	// count += printf("this is orig %p %p\n", ptr, p);
-	// mine += ft_printf("this is mine %s %s\n", "thy will be done", "42 is the best");
-	// count += printf("this is orig %s %s\n", "thy will be done", "42 is the best");
-	// mine += ft_printf("this is mine %i %d\n", 75385782, -88888);
-	// count += printf("this is orig %i %d\n", 75385782, -88888);
-	// mine += ft_printf("this is mine %u %u\n", 0001, 1164);
-	// count += printf("this is orig %u %u\n", 0001, 1164);
-	// mine += ft_printf("this is mine %c %c\n", 'o', 'g');
-	// count += printf("this is orig %c %c\n", 'o', 'g');
-
+	printf("Address = %p\n", p);
+	mine += ft_printf("this is mine %p\n", p);
+	count += printf("this is orig %p\n", p);
+	printf("org = %i\n", count);
+	mine += ft_printf("this is mine %X %X\n", 1234, -1234);
+	count += printf("this is orig %X %X\n", 1234, -1234);
+	mine += ft_printf("this is mine %x %x\n", 1234, -1234);
+	count += printf("this is orig %x %x\n", 1234, -1234);
+	mine += ft_printf("this is mine %p %p\n", ptr, p);
+	count += printf("this is orig %p %p\n", ptr, p);
+	mine += ft_printf("this is mine %s %s\n", "thy will be done", "42 is the best");
+	count += printf("this is orig %s %s\n", "thy will be done", "42 is the best");
+	mine += ft_printf("this is mine %i %d\n", 75385782, -88888);
+	count += printf("this is orig %i %d\n", 75385782, -88888);
+	mine += ft_printf("this is mine %u %u\n", 0001, 1164);
+	count += printf("this is orig %u %u\n", 0001, 1164);
+	mine += ft_printf("this is mine %c %c\n", 'o', 'g');
+	count += printf("this is orig %c %c\n", 'o', 'g');
 	printf("mine = %i\n", mine);
 	ft_printf("mine = %i\n", mine);
 	printf("org = %i\n", count);
 }
+*/
--- utils.c
+++ utils.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkoca <dkoca@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/08 16:36:09 by dkoca             #+#    #+#             */
+/*   Updated: 2024/01/08 16:36:14 by dkoca            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "printf.h"
 
 int	ft_strlen(const char *s)
@@ -39,28 +50,25 @@ int	putnbr_base(long int nb, char *base)
 	count = 0;
 	if (nb < 0 && base_len == 10)
 	{
-		print_char('-');
+		count += print_char('-');
 		count += putnbr_base(-nb, base);
 	}
 	else if (nb < 0 && base_len == 16)
-	{
-		count--;
 		count += putnbr_base((unsigned int)nb, base);
-	}
 	else if (nb >= base_len)
 	{
 		count += putnbr_base(nb / base_len, base);
-		print_char(base[nb % base_len]);
+		count += print_char(base[nb % base_len]);
 	}
 	else
-		print_char(base[nb % base_len]);
-	return (count + 1);
+		count += print_char(base[nb % base_len]);
+	return (count);
 }
 
 int	print_pointer(uintptr_t ptr, char *base, int flag)
 {
-	int base_len;
-	int count;
+	int	base_len;
+	int	count;
 
 	if (!ptr && flag)
 		return (print_str("(nil)"));
@@ -68,16 +76,15 @@ int	print_pointer(uintptr_t ptr, char *base, int flag)
 	count = 0;
 	if (flag)
 	{
-		print_str("0x");
-		count++;
+		count += print_str("0x");
 		count += print_pointer(ptr, base, 0);
 	}
 	else if (ptr >= base_len)
 	{
 		count += print_pointer(ptr / base_len, base, 0);
-		print_char(base[ptr % base_len]);
+		count += print_char(base[ptr % base_len]);
 	}
 	else
-		print_char(base[ptr % base_len]);
-	return (count + 1);
+		count += print_char(base[ptr % base_len]);
+	return (count);
 }
